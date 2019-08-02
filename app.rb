8c6975e3037caea191cbfa6b376ff9d00e7c2e97
Paul Rosero <paul_rosero@yahoo.com>
2019-08-02 17:22:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Paul"
+  end
+  
+  get'/hometown' do
+    "My hometown is Corona."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Last Resort"
+  end
 end
